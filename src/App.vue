50f5b4fce349d0c69f5b17aa6a1252e7b175a72a
Californ1a <rk_california@yahoo.com>
2023-11-17 01:27:44 -0500

    basic rules modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <GithubCorner />
   <main>
-    <h1>Chroma Chains</h1>
+    <div class="header">
+      <h1>Chroma Chains</h1>
+      <button type="button" @click="showModal = true">Show Rules</button>
+    </div>
     <GameBoard />
     <div class="controls">
       <DifficultySelector />
       <NewGameButton />
     </div>
+    <RulesModal v-model:showModal="showModal" />
   </main>
 </template>
 
@@ -15,9 +19,42 @@ import GameBoard from '@/components/GameBoard.vue';
 import GithubCorner from '@/components/GithubCorner.vue';
 import DifficultySelector from '@/components/DifficultySelector.vue';
 import NewGameButton from '@/components/NewGameButton.vue';
+import RulesModal from '@/components/RulesModal.vue';
+import { ref } from 'vue';
+
+const showModal = ref(false);
 </script>
 
 <style scoped>
+.header {
+  width: 26vw;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.padder {
+  width: 123px;
+}
+
+.header button {
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  border: none;
+  background: #eee;
+  color: #333;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background 0.2s ease;
+  margin-bottom: 0.5rem;
+}
+
+.header button:hover {
+  background: #ddd;
+}
+
 main {
   background: #333;
   height: 100%;
---
+++ src/components/RulesModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="modal" v-if="showModal" @click="closeModal">
+    <div class="modal-content" @click.stop>
+      <h2>Game Rules</h2>
+      <p>Each color on the game board has two nodes: a start and an end. Your task is to connect each pair of same-color nodes.</p>
+
+      <h2>Game Goal</h2>
+      <p>The goal of the game is to fill the entire board with wires. Each pair of same-color nodes must be connected with a wire, and all the cells on the board must be filled.</p>
+
+      <h2>Controls</h2>
+      <p>Click on a node and drag along the board until you reach another node of the same color to create a wire. If you want to remove a wire, simply click on it and it will be removed, allowing you to re-place the wire.</p>
+
+      <h2>Other Game Info</h2>
+      <p>Remember, wires of different colors can't cross each other. Plan your moves carefully to avoid getting stuck!</p>
+
+      <button type="button" @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  showModal: Boolean,
+});
+
+const emit = defineEmits(['update:showModal']);
+
+const closeModal = () => {
+  emit('update:showModal', false);
+};
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #000;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+button {
+  color: white;
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  border: none;
+  cursor: pointer;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+</style>
